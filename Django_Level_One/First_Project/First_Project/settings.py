252bd26de1adf28ef879d11da8a98108caaa7ab3
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2020-07-21 23:44:23 +0530

    End of Django_Level_One

--- Django_Level_One/First_Project/First_Project/settings.py
+++ Django_Level_One/First_Project/First_Project/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 templates_dir = os.path.join(BASE_DIR,"Templates")
+static_dir = os.path.join(BASE_DIR,"Static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    static_dir,
+]
