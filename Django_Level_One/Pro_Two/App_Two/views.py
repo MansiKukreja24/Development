ecbcdf90c030221b8509df76eb854a9e63274b4c
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2020-07-21 22:55:46 +0530

    Templates Challenge

--- Django_Level_One/Pro_Two/App_Two/views.py
+++ Django_Level_One/Pro_Two/App_Two/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<em>My Second App</em>")
+
+def help(request):
+    help_dict = {'insert_help':"Help Page"}
+    return render(request,'App_Two/help.html',context=help_dict)
