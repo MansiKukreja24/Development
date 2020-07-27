0faa095c7272954d976c64eef311485092e56b42
MansiKukreja24 <55954224+MansiKukreja24@users.noreply.github.com>
2020-07-28 00:40:20 +0530

    Level Two project

---
+++ Django_Level_Two/Pro_Two/app_two/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from app_two.models import User
+
+# Create your views here.
+
+def index(request):
+    return render(request,'app_two/index.html')
+
+def users(request):
+
+    user_list = User.objects.order_by('first_name')
+    user_dict = {"users":user_list}
+    return render(request,'app_two/users.html',context=user_dict)
